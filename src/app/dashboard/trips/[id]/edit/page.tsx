12cbdee1b48acf0eae46d3dc8292df0d90fa22c2
MohamadAlgazli <brawjym@gmail.com>
2025-03-10 00:07:47 +0300

    PageParams in EditTripPage

--- src/app/dashboard/trips/[id]/edit/page.tsx
+++ src/app/dashboard/trips/[id]/edit/page.tsx
@@ -1,11 +1,14 @@
 import { PageHeader } from "@/components/dashboard/page-header"
 import { TripForm } from "../../_components/trip-form"
+import { PageParams } from "@/types/page-params";
 
-export default function EditTripPage({ params }: { params: { id: string } }) {
+export default async function EditTripPage({ params }: PageParams<{ id: string }>) {
+  const { id } = await params;
+  
   return (
     <div className="space-y-6">
       <PageHeader title="Edit Trip" description="Update trip information" backButtonLink="/dashboard/trips" />
-      <TripForm id={params.id} />
+      <TripForm id={id} />
     </div>
   )
 }
