3bb5c944d30db0ddae0fdeee9afb482b5f21f0ca
MohamadAlgazli <brawjym@gmail.com>
2025-06-17 18:05:22 +0300

    add script tags for SEO

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -11,8 +11,10 @@ import { routing } from "@/i18n/routing";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { PageParams } from "@/types/page-params";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import Providers from "./providers";
+import Script from "next/script";
+import { firstDataLayerScript, googleTagManagerScript, secondDataLayerScript, yandexCounterScript } from "./scripts";
 
 export const metadata: Metadata = {
   title: "Karim Tour",
@@ -40,7 +42,35 @@ export default async function RootLayout({
       className={`${GeistSans.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-17215052073" />
+        <Script>{firstDataLayerScript}</Script>
+
+        <Script>{googleTagManagerScript}</Script>
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J2HS403HD2" />
+        <Script>{secondDataLayerScript}</Script>
+
+        <Script>{yandexCounterScript}</Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/102714145"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PTKXXBPK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider>
           <Providers locale={locale}>
             <Suspense>
