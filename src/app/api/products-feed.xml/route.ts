8445fdc572f05960b4e25e2e7c458197dd9d404c
karim-abdo83 <kiwitraveleg@gmail.com>
2025-06-25 20:40:45 +0300

    Update route.ts

--- src/app/api/products-feed.xml/route.ts
+++ src/app/api/products-feed.xml/route.ts
@@ -2,20 +2,23 @@ import { NextResponse } from 'next/server';
 import { api } from '@/src/trpc/server';
 
 function escapeXml(unsafe: string) {
-  return unsafe.replace(/[<>&"']/g, (c) => {
-    switch (c) {
-      case '<': return '&lt;';
-      case '>': return '&gt;';
-      case '&': return '&amp;';
-      case '"': return '&quot;';
-      case "'": return '&apos;';
-      default: return c;
-    }
-  });
+  return unsafe
+    .replace(/[<>&"']/g, (c) => {
+      switch (c) {
+        case '<': return '&lt;';
+        case '>': return '&gt;';
+        case '&': return '&amp;';
+        case '"': return '&quot;';
+        case "'": return '&apos;';
+        default: return c;
+      }
+    })
+    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F-\u009F\uD800-\uDFFF\uFFFE\uFFFF]/g, '')
+    .replace(/[\u{110000}-\u{FFFFFFFF}]/gu, '');
 }
 
 async function getAllTrips() {
-  const allTrips = [];
+  const allTrips: any[] = [];
   let page = 0;
   const pageSize = 10;
 
@@ -48,18 +51,18 @@ export async function GET() {
   const trips = await getAllTrips();
 
   const items = trips.map((trip: any) => `
-  <item>
-    <g:id>${trip.id}</g:id>
-    <g:title>${escapeXml(String(trip.title))}</g:title>
-    <g:description>${escapeXml(String(trip.description))}</g:description>
-    <g:link>https://karimtor.com/ru/trips/${trip.id}</g:link>
-    <g:image_link>${escapeXml(trip.assetsUrls?.[0] || 'https://karimtor.com/logo.svg')}</g:image_link>
-    <g:availability>in stock</g:availability>
-    <g:price>${(trip.adultTripPriceInCents / 100).toFixed(2)} USD</g:price>
-    <g:brand>Karim Tour</g:brand>
-    <g:condition>new</g:condition>
-  </item>
-`).join("\n");
+    <item>
+      <g:id>${escapeXml(String(trip.id))}</g:id>
+      <g:title>${escapeXml(String(trip.title))}</g:title>
+      <g:description>${escapeXml(String(trip.description))}</g:description>
+      <g:link>https://karimtor.com/ru/trips/${trip.id}</g:link>
+      <g:image_link>${trip.assetsUrls?.[0] ? escapeXml(trip.assetsUrls[0]) : 'https://karimtor.com/logo.svg'}</g:image_link>
+      <g:availability>in stock</g:availability>
+      <g:price>${(trip.adultTripPriceInCents / 100).toFixed(2)} USD</g:price>
+      <g:brand>Karim Tour</g:brand>
+      <g:condition>new</g:condition>
+    </item>
+  `).join("\n");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
